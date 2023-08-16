9f4a4a177bc511683fe5ff42a07c0cc0241fedb6
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-08-16 17:29:36 +0100

    files update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { CartProvider } from "./context/CartContext";
 import "./App.css";
 import Items from "./components/Items";
-
-import CartPrice from "./context/CartPrice";
 
 function App() {
   return (
     <div style={{ display: "flex", gap: "15rem" }}>
       <div>
-        <CartProvider>
+        <Items />
+        {/* <Items />
           <Items />
-          <Items />
-          <Items />
-          <Items />
-        </CartProvider>
-      </div>
-      <div>
-        <CartPrice />
+          <Items /> */}
       </div>
     </div>
   );
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,56 +1,70 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useCart } from "../context/CartContext"; // Adjust the path as needed
+import { useEffect, useState, createContext } from "react";
+// import "./ProductList.css";
+import Cart from "./Cart";
 
-const dummy = {
-  title: "iPhone",
-  price: "$Price",
-  description: "Description",
-};
+export const CartItemContext = createContext(0);
+export const RemoveItemContext = createContext([]);
 
 function Items() {
-  const { setTotalItems, setTotalPrice } = useCart();
-
-  const [productData, setProductData] = useState(dummy);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    const fetchProductData = async () => {
-      try {
-        const response = await fetch("https://dummyjson.com/products/");
-        const data = await response.json();
-        // console.log(data);
-        const updatedProductData = data.map((product) => ({
-          title: product.title,
-          image: product.images[0], // You should check if "images" exists in each product
-          price: product.price,
-          description: product.description,
-        }));
-        console.log(updatedProductData);
-        setProductData(updatedProductData);
-        console.log(updatedProductData);
-      } catch (error) {
-        console.error("Error fetching product data:", error);
-      }
-    };
-
-    fetchProductData();
+    fetch("https://dummyjson.com/products/")
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products))
+      .catch((error) => console.log("Error fetching products" + error));
   }, []);
+  console.log(products);
 
-  const addToCart = () => {
-    setTotalItems((prevTotalItems) => prevTotalItems + 1);
-    setTotalPrice(
-      (prevTotalPrice) =>
-        prevTotalPrice + parseFloat(productData.price.replace("$", ""))
-    );
+  const addToCart = (product) => {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      setCartItems((prevCartItems) =>
+        prevCartItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (productId) => {
+    setCartItems(cartItems.filter((item) => item.id !== productId));
   };
 
   return (
-    <div>
-      <h1>{productData.title}</h1>
-      <p>{productData.description}</p>
-      <p>{productData.price}</p>
-      <button onClick={addToCart}>Add to cart</button>
-    </div>
+    <CartItemContext.Provider value={cartItems}>
+      <RemoveItemContext.Provider value={removeFromCart}>
+        <div className="product-align">
+          <div className="product-list">
+            {products.map((product) => (
+              <div key={product.id} className="product">
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="product-image"
+                />
+                <h2 className="product-title">{product.title}</h2>
+                <p className="product-description">{product.description}</p>
+                <p className="product-price">${product.price.toFixed(2)}</p>
+                <button
+                  className="add-to-cart-button"
+                  onClick={() => addToCart(product)}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            ))}
+          </div>
+          <Cart />
+        </div>
+      </RemoveItemContext.Provider>
+    </CartItemContext.Provider>
   );
 }
 
